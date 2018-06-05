1c11347c9c518e909c184ccc3a4dd4b07ff114b8
Genki Shimada <te108600@gmail.com>
2018-06-05 10:10:45 +0000

    gja

--- scripts/buzzer3.py
+++ scripts/buzzer3.py
@@ -5,7 +5,7 @@ from std_msgs.msg import UInt16
 def write_freq(hz=0):
 	bfile = "/dev/rtbuzzer0"
 	try:
-		with open(bfile,"W") as f:
+		with open(bfile,"w") as f:
 			f.write(str(hz) + "\n")
 	except IOError:
 		rospy.logerr("can't write to " + bfile)
